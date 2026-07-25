ea4e172467baba69062dc9454b32b62504af748a
Stephan <61632585+eyshenrory@users.noreply.github.com>
2026-07-25 16:11:48 +0300

    Add star scheme

--- sql/guard_check.sql
+++ sql/guard_check.sql
@@ -3,4 +3,12 @@ SELECT
      WHERE rate_date = (SELECT MAX(rate_date) FROM staging_rates))
     OR
     (SELECT (MAX(rate_date) + INTERVAL '3 days') < CURRENT_DATE FROM staging_rates)
+    OR
+    ( 
+        (SELECT COUNT(*) FROM staging_rates
+        WHERE rate_date = (SELECT MAX(rate_date) FROM staging_rates))
+        !=
+        (SELECT COUNT(*) FROM fct_daily_rates
+        WHERE date_key = (SELECT MAX(date_key) FROM fct_daily_rates))
+    )
     AS pipeline_failed;
