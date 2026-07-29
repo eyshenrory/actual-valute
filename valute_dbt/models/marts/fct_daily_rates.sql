b410f7e773f30ba75a0eb6315bcdb43b069cb47c
Stephan Ilinykh <w1llowell@ya.ru>
2026-07-29 03:37:12 +0300

    cleaning

--- valute_dbt/models/marts/fct_daily_rates.sql
+++ valute_dbt/models/marts/fct_daily_rates.sql
@@ -1,19 +1,17 @@
 {{ config(
     materialized='incremental',
     unique_key=['rate_date', 'char_code']
-)
-}}
+) }}
 
 select
-    sr.rate_date,
-    dc.char_code,
-    sr.value,
-    sr.previous,
-    sr.nominal,
-    sr.value / sr.nominal as rate_per_unit
-from {{ ref('stg_cbr__rates') }} sr
-join {{ ref('dim_currency') }} dc on dc.char_code = sr.char_code
-join {{ ref('dim_date') }} dd on dd.full_date  = sr.rate_date
+    rate_date,
+    char_code,
+    value,
+    previous,
+    nominal,
+    value / nominal as rate_per_unit
+from {{ ref('stg_cbr__rates') }}
+
 {% if is_incremental() %}
-    where sr.rate_date >= (select max(rate_date) from {{ this }}) - interval '3 days'
+    where rate_date >= (select max(rate_date) from {{ this }}) - interval '3 days'
 {% endif %}
